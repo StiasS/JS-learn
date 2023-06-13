b3d9531682235c1dc292d085a5db9a074ca55f35
StiasS <stiass3110@gmail.com>
2023-06-13 16:29:03 +0300

    redacted

--- 2/2.13_task7__3lvl.js
+++ 2/2.13_task7__3lvl.js
@@ -1,7 +1,8 @@
-let n=prompt("Введите любой число");
-slojna:for(let i=2;i<=n;i++){
-    for(let k=2;k<i;k++){
-        if(i%k==0)continue slojna
+let n = prompt("Введите любой число");
+slojna: for (let i=2;i<=n;i++) {
+    for (let k=2;k<i;k++){
+        if (i%k==0);
+        continue slojna;
     }
     alert(i)
 }
