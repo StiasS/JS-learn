94a994e2650e1b6a4367e1f3feba0bb74d59db6a
StiasS <stiass3110@gmail.com>
2023-06-29 05:26:11 +0300

    5.8

--- 5/5.8_task1__4lvl.js
+++ 5/5.8_task1__4lvl.js
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <script>
-  let messages = [
+let messages = [
     {text: "Hello", from: "John"},
     {text: "How goes?", from: "John"},
     {text: "See you soon", from: "Alice"}
 ];
 let readmessages=new WeakSet();
-readmessages.add(messages[0]);
 readmessages.add(messages[1]);
 readmessages.add(messages[0]);
 alert("Read message 0: " + readMessages.has(messages[0]));
 messages.shift();
-  </script>
-</body>
-</html>
+alert(messages())
