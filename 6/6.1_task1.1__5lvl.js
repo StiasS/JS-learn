78fb933846066a9521ab13396df738ea18d72c07
StiasS <stiass3110@gmail.com>
2023-07-04 03:26:01 +0300

    6.1 complete

---
+++ 6/6.1_task1.1__5lvl.js
@@ -0,0 +1,8 @@
+function sumTo(n){
+    let z=0
+    for (let i=1 ; i<=n; i++) {
+        z+=i
+    }
+    alert (z)
+}
+sumTo(n)
---
+++ 6/6.1_task1.3__5lvl.js
@@ -0,0 +1,4 @@
+function sumTo(n){
+    return alert (n=(n*(n+1)/2))
+}
+sumTo(16)
