a917d98eb4d6094a621d2cdb10126be85489ea0b
StiasS <stiass3110@gmail.com>
2023-08-19 15:32:27 +0300

    9 comlete

--- 9/9.3_task1.1__5lvl.js
+++ 9/9.3_task1.1__5lvl.js
@@ -1,9 +1,10 @@
 class Rabbit extends Object {
     constructor(name) {
       super(name)
+      this.name=name
     }
   }
   
   let rabbit = new Rabbit("Кроль");
   
-  alert( rabbit.hasOwnProperty('name') ); // Ошибка
+  alert( rabbit.hasOwnProperty('name') )
