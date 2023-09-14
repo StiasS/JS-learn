e44739ee3cc80e5de9afbaf5a76487814647c035
StiasS <stiass3110@gmail.com>
2023-09-14 16:55:16 +0300

    sdelal

--- Experement/js.js
+++ Experement/js.js
@@ -1,30 +1,45 @@
 
 
-  function go() {
-    showCircle(160, 110, 39).then(div => {
-      div.classList.add('message-ball');
-      div.append("");
-    });
-  }
+let goCount = 0;
 
-  function showCircle(cx, cy, radius) {
-    let div = document.createElement('div');
-    div.style.width = 0;
-    div.style.height = 0;
-    div.style.left = cx + 'px';
-    div.style.top = cy + 'px';
-    div.className = 'circle';
-    document.body.append(div);
+function go() {
+  goCount++;
 
-    return new Promise(resolve => {
-      setTimeout(() => {
-        div.style.width = radius * 2 + 'px';
-        div.style.height = radius * 2 + 'px';
+  showCircle(160, 110, 39).then(div => {
+    div.classList.add('message-ball');
+    div.append("");
+    if (goCount >= 10) {
+      goCount=0
+      removeCircles();
+    }
+  });
+}
 
-        div.addEventListener('transitionend', function handler() {
-          div.removeEventListener('transitionend', handler);
-          resolve(div);
-        });
-      }, 0);
-    })
-  }
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  });
+}
+
+function removeCircles() {
+  let circles = document.querySelectorAll('.circle');
+  circles.forEach(circle => {
+    circle.remove();
+  });
+}
